aef7f508d7098e2ee5ff58ad23bb82764d6018e2
LAPTOP-SB0CM2VU\Playdata <wjdxodndjaak@naver.com>
2024-01-22 18:28:36 +0900

    24-01-22
    자바 컬렉션, 제네릭스

---
+++ chap10-generics-lecture-source/src/main/java/com/ohgiraffers/section02/extend/vo/RabbitFarm.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section02.extend.vo;
+
+public class RabbitFarm<T extends Rabbit> {
+    private T animal;
+
+    public RabbitFarm() {
+    }
+
+    public T getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(T animal) {
+        this.animal = animal;
+    }
+}
